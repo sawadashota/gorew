c6a9166c993b4d66609fe7aee2122667304c2ca2
Shota Sawada <shota.sawada@crowdworks.co.jp>
2018-05-01 15:42:50 +0900

    Add completion command

---
+++ completion/completion.go
@@ -0,0 +1,34 @@
+package completion
+
+import "github.com/posener/complete"
+
+func Run() {
+	gocmd := complete.Command{
+		Sub: complete.Commands{
+			"install": complete.Command{
+				Flags: complete.Flags{
+					"-f":     complete.PredictDirs("*"),
+					"--file": complete.PredictDirs("*"),
+				},
+			},
+			"init": complete.Command{
+				Flags: complete.Flags{
+					"-b":        complete.PredictDirs("*"),
+					"--binPath": complete.PredictDirs("*"),
+					"-f":        complete.PredictDirs("*"),
+					"--file":    complete.PredictDirs("*"),
+					"-s":        complete.PredictDirs("*"),
+					"--srcPath": complete.PredictDirs("*"),
+				},
+			},
+		},
+		Flags: complete.Flags{
+			"help": complete.PredictNothing,
+		},
+		GlobalFlags: complete.Flags{
+			"--help": complete.PredictNothing,
+			"-h":     complete.PredictNothing,
+		},
+	}
+	complete.New("gocmd", gocmd).Run()
+}
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/sawadashota/gocmd/cmd"
+import (
+	"github.com/sawadashota/gocmd/cmd"
+	"github.com/sawadashota/gocmd/completion"
+)
 
 func main() {
 	cmd.Commands().Execute()
+	completion.Run()
 }
