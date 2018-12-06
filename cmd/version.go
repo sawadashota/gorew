67088d8e41d3c76c90cbe3af5346cc07b6bdde87
Shota SAWADA <xiootas@gmail.com>
2018-12-06 23:46:46 +0900

    using goreleaser var

--- cmd/version.go
+++ cmd/version.go
@@ -2,17 +2,14 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	version string
-	gitHash string
-
-	TimeNow time.Time
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 var versionCmd = &cobra.Command{
@@ -20,31 +17,11 @@ var versionCmd = &cobra.Command{
 	Short: "Display this binary's version, build time and git hash of this build",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("Version:	%s\n", version)
-		fmt.Printf("Git Hash:	%s\n", gitHash)
-		fmt.Printf("Build Time:	%s\n", timeNow().UTC().Format("2006-01-02 15:04:05 MST"))
+		fmt.Printf("Git Hash:	%s\n", commit)
+		fmt.Printf("Build Time:	%s\n", date)
 	},
 }
 
 func init() {
-	version = getStringEnv("CIRCLE_TAG", "dev-master")
-	gitHash = getStringEnv("CIRCLE_SHA1", "undefined")
 	RootCmd.AddCommand(versionCmd)
-}
-
-// getStringEnv returns environment variable
-// if the key doesn't exist, return default value
-func getStringEnv(key string, defaultValue string) string {
-	v := os.Getenv(key)
-	if v == "" {
-		return defaultValue
-	}
-	return v
-}
-
-func timeNow() time.Time {
-	if &TimeNow != nil {
-		return TimeNow
-	}
-
-	return time.Now()
 }
