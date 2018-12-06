fb5dcc18f3f14107369a69262daddec26e9b4ad3
Shota SAWADA <xiootas@gmail.com>
2018-12-06 23:58:17 +0900

    fix version

--- cmd/version.go
+++ cmd/version.go
@@ -2,14 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
+	version string
+	gitHash string
+
+	TimeNow time.Time
 )
 
 var versionCmd = &cobra.Command{
@@ -17,11 +20,31 @@ var versionCmd = &cobra.Command{
 	Short: "Display this binary's version, build time and git hash of this build",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("Version:	%s\n", version)
-		fmt.Printf("Git Hash:	%s\n", commit)
-		fmt.Printf("Build Time:	%s\n", date)
+		fmt.Printf("Git Hash:	%s\n", gitHash)
+		fmt.Printf("Build Time:	%s\n", time.Now().UTC().Format("2006-01-02 15:04:05 MST"))
 	},
 }
 
 func init() {
+	version = getStringEnv("VERSION", "dev-master")
+	gitHash = getStringEnv("Git_HASH", "undefined")
 	RootCmd.AddCommand(versionCmd)
+}
+
+// getStringEnv returns environment variable
+// if the key doesn't exist, return default value
+func getStringEnv(key string, defaultValue string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		return defaultValue
+	}
+	return v
+}
+
+func timeNow() time.Time {
+	if &TimeNow != nil {
+		return TimeNow
+	}
+
+	return time.Now()
 }
